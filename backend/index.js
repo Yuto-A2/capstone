0f83bb0f0280e1c1def419694163b0c38d6536eb
Yuto Arimori <n01648493@humber.ca>
2024-09-28 19:47:22 -0400

    Added new files in routes folder.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const router = require("express").Router();
 const cors = require("cors")
 const { MongoClient } = require("mongodb");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 const port = process.env.PORT || "5173";
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 const dbUrl = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPWD}@${process.env.DBHOST}?retryWrites=true&w=majority&appName=Portfolio`;
 const client = new MongoClient(dbUrl);
+
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
@@ -15,6 +20,9 @@ app.use(cors({
     origin: "*"
 }));
 
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 app.get("/api/login", async (request, response) => {
     let login = await getloginInfo();
     response.json(login);
