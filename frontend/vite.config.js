91b05383cf050bfb2f3e3401d25328420d2f602b
Yuto Arimori <n01648493@humber.ca>
2024-10-15 14:45:55 -0400

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,14 +12,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // エイリアスの設定
-    },
-  },
   build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // ここをエントリーポイントに指定
-    }
+    outDir: 'dist'
   }
 });
