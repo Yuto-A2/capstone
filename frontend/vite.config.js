abbb069ba998a4701320425d19b2905b2b6b5ca1
Yuto Arimori <n01648493@humber.ca>
2024-10-15 13:28:17 -0400

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: './src/main.jsx', // ここをエントリーポイントに指定
+      input: '/src/main.jsx', // ここをエントリーポイントに指定
     }
   }
 });
