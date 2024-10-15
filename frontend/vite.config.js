78ddbecf5930b527de558f2c9d2f9a6e5c2295d6
Yuto Arimori <n01648493@humber.ca>
2024-10-15 12:26:40 -0400

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'your/deploy/directory', 
   },rollupOptions: {
     external: ['some-library']  // ここに外部化するモジュールを指定します
   }
